1433ba9edd3930d158bf0db23ff1598efeed9fe5
Crowdbotics <team@crowdbotics.com>
2021-08-24 02:12:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quadric_30063Identifier",
+  url: "https://quadric_30063Identifier.botics.co",
+  api: "https://quadric_30063Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
